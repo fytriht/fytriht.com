7cfc4aeec4e683c20ef558f848b9b8f0dcda7062
Zhang Zhi <fytriht@gmail.com>
2024-01-07 16:34:03 +0800

    Query item styling

--- app/url-editor/label-wrap.tsx
+++ app/url-editor/label-wrap.tsx
@@ -1,14 +1,15 @@
-import { ReactElement, ReactNode } from "react";
+import { CSSProperties, ReactElement, ReactNode } from "react";
 import { Label } from "../../components/label";
 
 export type LabelWrapProps = React.PropsWithChildren<{
   label: ReactNode;
   htmlFor?: string;
+  style: CSSProperties;
 }>;
 
-export function LabelWrap({ children, label, htmlFor }: LabelWrapProps) {
+export function LabelWrap({ children, label, htmlFor, style }: LabelWrapProps) {
   return (
-    <div className="flex items-center">
+    <div className="flex items-center" style={style}>
       <Label className="w-32" htmlFor={htmlFor}>{`${label}: `}</Label>
       {children}
     </div>
--- app/url-editor/query-editor.tsx
+++ app/url-editor/query-editor.tsx
@@ -3,6 +3,7 @@ import { UrlEditor } from "./url-editor";
 import { canParsed } from "./url-utils";
 import { Input } from "../../components/input";
 import { LabelWrap } from "./label-wrap";
+import { Label } from "../../components/label";
 
 export function QueryEditor({
   query,
@@ -15,34 +16,48 @@ export function QueryEditor({
   return (
     <div className="w-full space-y-1">
       {[...searchParams.entries()].map(([key, value], idx) => {
+        if (canParsed(value)) {
+          return (
+            <div
+              key={key + idx}
+              className="flex"
+              style={{
+                backgroundColor: "#f1f5fa",
+                borderRadius: "calc(var(--radius) - 2px)",
+              }}
+            >
+              <Label className="w-32">{`${key}: `}</Label>
+              <UrlEditor
+                className="flex flex-col w-full"
+                url={value}
+                onChange={(v) => {
+                  const searchObj = new URLSearchParams(searchParams);
+                  searchObj.set(key, v);
+                  onChange?.(searchObj.toString());
+                }}
+              />
+            </div>
+          );
+        }
+
         return (
-          <LabelWrap key={key + idx} label={key}>
-            {(() => {
-              if (canParsed(value)) {
-                return (
-                  <UrlEditor
-                    className="flex flex-col w-full"
-                    url={value}
-                    onChange={(v) => {
-                      const searchObj = new URLSearchParams(searchParams);
-                      searchObj.set(key, v);
-                      onChange?.(searchObj.toString());
-                    }}
-                  />
-                );
-              }
-              return (
-                <Input
-                  value={value}
-                  onChange={(e) => {
-                    const { value } = e.target;
-                    const searchObj = new URLSearchParams(searchParams);
-                    searchObj.set(key, value);
-                    onChange?.(searchObj.toString());
-                  }}
-                />
-              );
-            })()}
+          <LabelWrap
+            key={key + idx}
+            label={key}
+            style={{
+              backgroundColor: "#f1f5fa",
+              borderRadius: "calc(var(--radius) - 2px)",
+            }}
+          >
+            <Input
+              value={value}
+              onChange={(e) => {
+                const { value } = e.target;
+                const searchObj = new URLSearchParams(searchParams);
+                searchObj.set(key, value);
+                onChange?.(searchObj.toString());
+              }}
+            />
           </LabelWrap>
         );
       })}
